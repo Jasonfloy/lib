227669082bd51c44205ad172e66ac44a7a1a6803
bigzhu <vermiliondun@gmail.com>
2014-11-12 18:09:21 +0800

    勉强实现无极评论

--- tornado_ui_bz.py
+++ tornado_ui_bz.py
@@ -36,6 +36,11 @@ class MenuLink(tornado.web.UIModule):
     def render(self, menu_link):
         return self.render_string("template_bz/MenuLink.html", **menu_link)
 
+class CommentReply(tornado.web.UIModule):
+    def render(self, comments, timeLen):
+        return self.render_string("template_bz/CommentReply.html", comments=comments, timeLen=timeLen)
+
+
 
 class Comment(tornado.web.UIModule):
 
@@ -44,6 +49,147 @@ class Comment(tornado.web.UIModule):
     comments: 评论组, 包括评论内容,以及评论时间
     id: 评论附着于哪个东西, 这个东西的 id
     '''
+    def embedded_css(self):
+        return '''
+.comment-list {
+position: relative;
+}
+.comment-list:before {
+position: absolute;
+top: 0;
+bottom: 35px;
+left: 18px;
+width: 1px;
+background: #e0e4e8;
+content: '';
+}
+.comment-list .comment-item {
+margin-top: 0;
+position: relative;
+padding-left: 40px;
+}
+.comment-list .thumb-sm {
+width: 36px;
+}
+.avatar {
+position: relative;
+display: block;
+border-radius: 5px;
+white-space: nowrap;
+margin-right: 10px;
+margin-left: -40px;
+}
+.comment-list .arrow.left {
+top: 20px;
+left: 39px;
+}
+.comment-list .comment-reply .arrow.left {
+top: 20px;
+left: -1px;
+}
+.arrow.left {
+top: 50%;
+left: -8px;
+margin-top: -8px;
+border-left-width: 0;
+border-right-color: #eee;
+border-right-color: rgba(0,0,0,0.1);
+}
+.arrow:after {
+border-width: 7px;
+content: "";
+}
+.arrow, .arrow:after {
+position: absolute;
+display: block;
+width: 0;
+height: 0;
+border-color: transparent;
+border-style: solid;
+}
+.arrow.left:after {
+content: " ";
+left: 1px;
+border-left-width: 0;
+border-right-color: #fff;
+bottom: -7px;
+}
+.arrow {
+border-width: 8px;
+z-index: 10;
+}
+.comment-list .comment-item .comment-body {
+margin-left: 6px;
+}
+.panel.panel-default {
+border-color: #eaeef1;
+}
+.panel {
+border-radius: 2px;
+}
+.comment-list .comment-item .panel-heading, .comment-list .comment-item .panel-footer {
+position: relative;
+font-size: 12px;
+background: #fff;
+}
+.bg-white a {
+color: #545a5f;
+}
+.thumb img, .thumb-xs img, .thumb-sm img, .thumb-md img, .thumb-lg img, .thumb-btn img {
+height: auto;
+max-width: 100%;
+vertical-align: middle;
+}
+.avatar img {
+border-radius: 5px;
+width: 100%;
+}
+.bg-info {
+background-color: #4cb6cb;
+color: #eaf6f9;
+}
+.bg-dark {
+background-color: #5a6a7a;
+color: #c9d0d7;
+}
+.bg-success {
+background-color: #1ab667;
+color: #a9f3ce;
+}
+.badge, .label {
+font-weight: bold;
+}
+.label {
+display: inline;
+padding: .2em .6em .3em;
+font-size: 75%;
+font-weight: bold;
+line-height: 1;
+color: #fff;
+text-align: center;
+white-space: nowrap;
+vertical-align: baseline;
+border-radius: .25em;
+}
+.comment-list .comment-reply {
+margin-left: 46px;
+position: relative;
+}
+.input-group {
+position: relative;
+display: table;
+border-collapse: separate;
+}
+.input-group-addon, .input-group-btn, .input-group .form-control {
+display: table-cell;
+}
+.input-group .form-control {
+position: relative;
+z-index: 2;
+float: left;
+width: 100%;
+margin-bottom: 0;
+}    '''
 
     def javascript_files(self):
         '''
