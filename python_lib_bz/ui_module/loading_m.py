61dcc45c7139c70106e710eee913e41ee25df186
bigzhu <vermiliondun@gmail.com>
2015-02-21 01:59:35 +0800

    loading 用 ui_module 来封装

---
+++ python_lib_bz/ui_module/loading_m.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from . import my_ui_module
+class loading_m(my_ui_module.MyUIModule):
+    '''create by bigzhu at 15/02/20 23:20:30 loading 的 module
+    '''
+
+
+    def render(self):
+        return self.render_string(self.html_name)
+
+if __name__ == '__main__':
+    pass
