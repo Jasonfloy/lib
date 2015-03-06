30ecf34cf113cd415758fb972fe8ac483758b1c2
ywq <ywq@highwe.com>
2015-03-06 16:38:19 +0800

    增加模块　浏览器检测到ｉｅ９以下版本，则会提示安装ｃｈｒｏｍｅ内核的浏览器．

---
+++ python_lib_bz/ui_module/check_ie_m.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ui_module import my_ui_module
+
+
+class check_ie_m(my_ui_module.HtmlUIModule):
+
+    '''create by ywq at 15/03/06 判断低于ie9版本，则显示提示升级浏览器的页面
+    '''
+    pass
+
+if __name__ == '__main__':
+    pass
