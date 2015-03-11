89c4f28127de6e177cf95926caf4d18cf67178c2
bigzhu <vermiliondun@gmail.com>
2015-03-11 14:44:05 +0800

    实现了排序

--- python_lib_bz/ui_module/crud_m.py
+++ python_lib_bz/ui_module/crud_m.py
@@ -29,6 +29,7 @@ class CrudOper:
         ''' % table_name
         if isTime:
             sql += " and c_type='timestamp' "
+        sql += " order by seq, create_date"
         return list(self.pg.db.query(sql))
 
     def getCrudListConf(self, table_name, isTime=None):
