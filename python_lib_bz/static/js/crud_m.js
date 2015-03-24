17d520e35e2aed50f62f5c415bd6f0b67bb366be
ZhangRGK <zr@highwe.com>
2015-03-24 16:21:45 +0800

    修正了提示信息的bug

--- python_lib_bz/static/js/crud_m.js
+++ python_lib_bz/static/js/crud_m.js
@@ -122,7 +122,7 @@
     });
     AllDone = function(d) {
       v_crud.$set("loading", false);
-      if (d.error === "0") {
+      if (d.error === 0) {
         return window.bz.showSuccess5('提交成功...正在返回列表');
       } else {
         return window.bz.showError5(d);
