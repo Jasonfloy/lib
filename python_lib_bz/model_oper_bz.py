3db34e7a66513736cee12bd6a91100e860df0efb
bigzhu <vermiliondun@gmail.com>
2015-04-07 16:12:15 +0800

    没有建立模型的时候,需要退出运行

--- python_lib_bz/model_oper_bz.py
+++ python_lib_bz/model_oper_bz.py
@@ -53,7 +53,9 @@ def createTable(Model, db_name, user=None, password=None):
     except peewee.OperationalError:
         print public_bz.getExpInfo()
         showDBCreate(db_name)
-        return
+        exit(1)
+
+
     table_name = Model.__name__
     if table_name != 'base':
         sql = '''
