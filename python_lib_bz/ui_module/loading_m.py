bde22b2404bbc7746b38980bd14c9c2d18aae03c
bigzhu <vermiliondun@gmail.com>
2015-04-11 08:06:03 +0800

    css 也要更新

--- python_lib_bz/ui_module/loading_m.py
+++ python_lib_bz/ui_module/loading_m.py
@@ -9,10 +9,15 @@ class loading_m(my_ui_module.MyUIModule):
     '''create by bigzhu at 15/02/20 23:20:30 loading 的 module
     create by bigzhu at 15/04/10 21:55:37 改回继承MyUIModule,加入版本号
     '''
+
     def render(self):
         return ''
+
     def javascript_files(self):
-        return self.js_file+'?v=5'
+        return self.js_file + '?v=5'
+
+    def css_files(self):
+        return self.css_file + '?v=1'
 
 if __name__ == '__main__':
     pass
