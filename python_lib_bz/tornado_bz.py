83cc73479b40b790b763cf8572467ef93536d2f0
bigzhu <vermiliondun@gmail.com>
2015-04-13 21:12:34 +0800

    add /

--- python_lib_bz/tornado_bz.py
+++ python_lib_bz/tornado_bz.py
@@ -307,7 +307,7 @@ def mustSubscribe(method):
                 "appid": self.settings['appid'],
                 #"redirect_uri": "http://" + self.settings['domain'] + "/setOpenid?url=/" + self.__class__.__name__,
                 #"redirect_uri": "http://" + self.settings['domain'] + "/setOpenid?url=" + self.request.uri,
-                "redirect_uri": "http://" + "admin.hoywe.com/court" + "?url=" + self.request.uri,
+                "redirect_uri": "http://" + "admin.hoywe.com/court" + "/?url=" + self.request.uri,
                 "response_type": "code",
                 "scope": "snsapi_base",
             }
