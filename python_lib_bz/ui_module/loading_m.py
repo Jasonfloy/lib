d5a3e10879b4c19b53b2d763c49f0baf0d8ad8e6
ZhangRGK <zr@highwe.com>
2015-04-14 13:37:32 +0800

    修改loading透明度

--- python_lib_bz/ui_module/loading_m.py
+++ python_lib_bz/ui_module/loading_m.py
@@ -14,10 +14,10 @@ class loading_m(my_ui_module.MyUIModule):
         return ''
 
     def javascript_files(self):
-        return self.js_file + '?v=6.2'
+        return self.js_file + '?v=6.3'
 
     def css_files(self):
-        return self.css_file + '?v=1'
+        return self.css_file + '?v=1.1'
 
 if __name__ == '__main__':
     pass
