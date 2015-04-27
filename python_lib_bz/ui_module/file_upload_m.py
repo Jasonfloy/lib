721c721004b8aa9b61b13434ffbffb554143b371
ZhangRGK <zr@highwe.com>
2015-04-27 16:44:36 +0800

    能够保存关联，能够看到已保存的文件

--- python_lib_bz/ui_module/file_upload_m.py
+++ python_lib_bz/ui_module/file_upload_m.py
@@ -30,8 +30,24 @@ class file_upload_m(my_ui_module.MyUIModule):
 class file_upload(UserInfoHandler):
     '''
     create by zhangrui at 15/03/12 14:50
-    文件上传处理方法
+    文件上传相关API
     '''
+    @tornado_bz.mustLogin
+    @tornado_bz.handleError
+    def get(self, parms_str):
+        '''
+        根据 table_name, column, id 获取已有文件
+        '''
+        parms = parms_str.split("/")
+        self.set_header("Content-Type", "application/json")
+        sql = """
+            select r.id, f.file_name, f.file_path, f.file_type, f.suffix, 'false' as remove
+            from uploaded_file_record_ref r left join uploaded_files f on r.uploaded_file_id = f.id
+            where r.ref_table = '%s' and r.ref_column = '%s' and r.ref_record_id = '%s' and r.is_delete != 1
+        """ % (parms[0], parms[1], parms[2])
+        results = list(self.pg.db.query(sql))
+        self.write(json.dumps({'error': 0, 'results': results}))
+
     @tornado_bz.mustLogin
     @tornado_bz.handleError
     def post(self):
@@ -74,29 +90,6 @@ class file_ref(UserInfoHandler):
     """
     @tornado_bz.mustLogin
     @tornado_bz.handleError
-    def get(self, offset, limit, file_name):
-        """
-        获取文件
-        url_mapping需要额外注册
-        /file_ref/offset/.(*)/limit/.(*)/filename/.(*)
-        """
-
-        sql = """
-        select id, file_name
-        from uploaded_files
-        where 1=1
-        """
-        if file_name:
-            sql += " and file like '%%s%'" % file_name
-        if offset:
-            sql += "\n offset %s" % offset
-        if limit:
-            sql += "\n limit %s" % limit
-        files = list(self.pg.db.query(sql))
-        self.write(json.dumps({"error": 0, "files": files}))
-
-    @tornado_bz.mustLogin
-    @tornado_bz.handleError
     def post(self):
         """
         保存文件关联
@@ -104,23 +97,21 @@ class file_ref(UserInfoHandler):
         self.set_header("Content-Type", "application/json")
         parms = json.loads(self.request.body)
         column = parms.get("column")
-        append_files = parms.get("append_files")
-        remove_files = parms.get("remove_files")
+        upload_ids = parms.get("upload_ids")
+        print upload_ids
         table_name = parms.get("table_name")
         record_id = parms.get("record_id")
         # 新增文件关联
-        if append_files:
-            for f in append_files:
+        if upload_ids:
+            for file_id in upload_ids:
                 file_ref = {
-                    "uploaded_file_id": f.get("file_id"),
+                    "uploaded_file_id": file_id,
                     "ref_table": table_name,
                     "ref_record_id": record_id,
                     "ref_column": column,
                     "create_user_id": self.get_current_user()
                 }
                 self.pg.db.insert("uploaded_file_record_ref", **file_ref)
-        if remove_files:
-            self.pg.db.update("uploaded_file_record_ref", where="id in (%s)" % ','.join(map(str, remove_files)), is_delete=1)
         self.write(json.dumps({"error": 0}))
 
 if __name__ == '__main__':
