8b848615f6c6459ec2a37d2e712e4c5be60e782c
zhangwh <zh.whong@gmail.com>
2015-05-12 14:58:26 +0800

    fix

--- python_lib_bz/static/js/crud_check_m.js
+++ python_lib_bz/static/js/crud_check_m.js
@@ -15,7 +15,6 @@
         getTableName: function() {
           var parm;
           parm = window.bz.getUrlParm();
-          console.log("parm: ", parm);
           return parm[2];
         },
         jump2List: function() {
@@ -23,21 +22,28 @@
           path = '/crud_check_list/' + this.getTableName();
           return location.pathname = path;
         },
-        save: function() {
-          var data, table_name;
-          data = this.$data;
-          data.loading = true;
+        save: function(checked) {
+          var _this, record, table_name;
+          _this = this;
+          this.loading = true;
           table_name = this.getTableName();
+          record = {
+            'id': this.record.id,
+            'checked': checked
+          };
+          console.log("record: ", record);
           return $.post('/crud_api', JSON.stringify({
             table_name: table_name,
-            record: data.record
-          })).done(function(result) {
-            if (result.error !== '0') {
-              return window.bz.showError5(result.error);
-            } else if (result.error === void 0) {
-              return data.error_info = '未知错误';
+            record: record
+          })).done(function(data) {
+            if (data.error === '0') {
+              window.bz.showSuccess5('审核成功...正在返回列表');
+              _this.loading = false;
+              return delay(2000, function() {
+                return _this.jump2List();
+              });
             } else {
-              return window.bz.showSuccess5('审核成功...正在返回列表');
+              return window.bz.showError5(data.error);
             }
           });
         },
@@ -52,23 +58,22 @@
             'id': id
           })).done(function(result) {
             var field, record;
-            console.log(result);
             if (result.error !== '0') {
-              window.bz.showError5(result.error);
+              return window.bz.showError5(result.error);
             } else {
               if (result.data.length > 0) {
                 record = result.data[0];
+                record.id = id;
                 for (field in record) {
                   if (record[field] !== null && typeof record[field] === "object") {
                     record[field] = JSON.stringify(record[field]);
                   }
                 }
-                _this.record = record;
+                return _this.record = record;
               } else if (id !== '') {
-                window.bz.showError5('未找到这条数据!');
+                return window.bz.showError5('未找到这条数据!');
               }
             }
-            return console.log("record:", _this.record);
           });
         }
       }
--- python_lib_bz/ui_module/crud_check_m.coffee
+++ python_lib_bz/ui_module/crud_check_m.coffee
@@ -12,26 +12,25 @@ $(->
         methods:
             getTableName:->
                 parm = window.bz.getUrlParm()
-                console.log "parm: ", parm
                 return parm[2]
 
             jump2List: ->
                 path = '/crud_check_list/' + @getTableName()
                 location.pathname = path
             
-            save: ->
-                data = @$data
-                data.loading=true
+            save:(checked) ->
+                _this = @
+                @loading = true
                 table_name = @getTableName()
-                $.post('/crud_api',
-                  JSON.stringify {table_name:table_name, record:data.record}
-                ).done((result)->
-                    if result.error!='0'
-                        window.bz.showError5(result.error)
-                    else if result.error == undefined
-                        data.error_info = '未知错误'
-                    else
+                record = {'id': @record.id, 'checked': checked}
+                console.log "record: ", record
+                $.post('/crud_api', JSON.stringify {table_name: table_name, record: record}).done((data)->
+                    if data.error == '0'
                         window.bz.showSuccess5('审核成功...正在返回列表')
+                        _this.loading = false
+                        delay 2000, -> _this.jump2List()
+                    else
+                        window.bz.showError5(data.error)
                 )
 
             loadData: ->
@@ -41,12 +40,12 @@ $(->
                 id = parm[0].replace('#','')
 
                 $.post('/crud_check', JSON.stringify({'table_name': table_name, 'id': id})).done((result)->
-                    console.log result
                     if result.error != '0'
                         window.bz.showError5(result.error)
                     else
                         if result.data.length > 0
                             record = result.data[0]
+                            record.id = id
                             for field of record
                                 if record[field] != null and typeof record[field] == "object"
                                     record[field] = JSON.stringify(record[field])
@@ -54,6 +53,5 @@ $(->
 
                         else if id != ''
                             window.bz.showError5('未找到这条数据!')
-                    console.log "record:", _this.record
                 )
 )
