dee66199d9be75f50048d1dedabba243a5b439ec
bigzhu <vermiliondun@gmail.com>
2015-05-17 11:26:41 +0800

    #3345

---
+++ python_lib_bz/test_bz.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+create by bigzhu at 15/05/17 10:53:17 用于将所有的module都调用一遍 doctest
+    只选出当前运行脚本的目录所有的module来测试
+'''
+import public_bz
+import pkgutil
+import doctest
+
+
+def add(a, b):
+    '''
+    >>> add(1,1)
+    2
+    '''
+    return a + b
+
+
+def testAll():
+    path = public_bz.getExecutingPath()
+    print 'starting test path:', path
+    for importer, modname, ispkg in pkgutil.iter_modules([path]):
+        if not ispkg:
+            module = importer.find_module(modname).load_module(modname)
+            doctest.testmod(module)
+if __name__ == '__main__':
+    pass
