09758c8a34e4a9c336149f649f7eb0a58d416290
bigzhu <vermiliondun@gmail.com>
2015-05-18 11:35:04 +0800

    print get_user_info token

--- python_lib_bz/tornado_bz.py
+++ python_lib_bz/tornado_bz.py
@@ -338,7 +338,7 @@ def mustSubscribe(method):
             return
         else:
             try:
-                print "get_user_info access_token = ", settings['access_token']
+                print "get_user_info access_token = ", self.settings['access_token']
                 wechat_user_info = self.wechat.get_user_info(openid, lang='zh_CN')
             except OfficialAPIError as e:
                 print e
