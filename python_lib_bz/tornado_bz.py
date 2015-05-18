681b28ce2b9bc9d3631ebe57f960bacc54ac6bff
bigzhu <vermiliondun@gmail.com>
2015-05-18 16:33:36 +0800

    check user_info

--- python_lib_bz/tornado_bz.py
+++ python_lib_bz/tornado_bz.py
@@ -336,36 +336,36 @@ def mustSubscribe(method):
             self.redirect(auth_url)
             return
         else:
-            exists_users = list(self.pg.db.select('wechat_user', where="openid='%s'" % openid))
-            if not exists_users:
-                try:
-                    print "get_user_info openid = ", openid
-                    wechat_user_info = self.wechat.get_user_info(openid, lang='zh_CN')
-                except OfficialAPIError as e:
-                    print public_bz.getExpInfoAll()
-                    self.clear_cookie(name='openid')
-                    error = public_bz.getExpInfo()
-                    if error.find('40001') != -1:
-                        raise e
-                    #else:
-                    #    error_info = '''
-                    #    <html>
-                    #        <script type="text/javascript">
-                    #        alert("微信服务器异常，请关闭后，重新打开");
-                    #        WeixinJSBridge.call('closeWindow');
-                    #        </script>
-                    #    </html>
-                    #    '''
-                    #    self.write(error_info)
-                    return
-
-                # 没有关注的,跳转到配置的关注页面
-                if wechat_user_info['subscribe'] == 0:
-                    self.redirect('http://' + self.settings["domain"] + self.settings["subscribe"])
-                    return
+            #exists_users = list(self.pg.db.select('wechat_user', where="openid='%s'" % openid))
+            #if not exists_users:
+            try:
+                print "get_user_info openid = ", openid
+                wechat_user_info = self.wechat.get_user_info(openid, lang='zh_CN')
+            except OfficialAPIError as e:
+                print public_bz.getExpInfoAll()
+                self.clear_cookie(name='openid')
+                error = public_bz.getExpInfo()
+                if error.find('40001') != -1:
+                    raise e
                 else:
-                    print 'add user'
-                    self.pg.db.insert('wechat_user', **wechat_user_info)
+                    error_info = '''
+                    <html>
+                        <script type="text/javascript">
+                        alert("微信服务器异常，请关闭后，重新打开");
+                        WeixinJSBridge.call('closeWindow');
+                        </script>
+                    </html>
+                    '''
+                    self.write(error_info)
+                return
+
+            # 没有关注的,跳转到配置的关注页面
+            if wechat_user_info['subscribe'] == 0:
+                self.redirect('http://' + self.settings["domain"] + self.settings["subscribe"])
+                return
+            else:
+                print 'add user'
+                self.pg.db.insert('wechat_user', **wechat_user_info)
 
         return method(self, *args, **kwargs)
     return wrapper
